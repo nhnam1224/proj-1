52c0b93e0a1af18a66139c8c7aa599aac9ea3fab
nhnam1224 <nguyenhainam10009000@gmail.com>
2026-04-12 22:11:30 +0700

    main stream control

---
+++ my-pixel-game/main.js
@@ -0,0 +1,24 @@
+import * as Phaser from 'phaser';
+import MenuScene from './src/scenes/MenuScene.js';
+import PauseScene from './src/scenes/PauseScene.js';
+import Room101Scene from './src/scenes/Floor1/Room101.js';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game-container',
+    pixelArt: true,
+    backgroundColor: '#050505',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+    // The order determines which scene starts first
+    scene: [MenuScene, Room101Scene, PauseScene]
+};
+
+new Phaser.Game(config);
