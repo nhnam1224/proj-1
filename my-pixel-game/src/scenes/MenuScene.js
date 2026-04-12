a44f0bdaa7404c7a0f13314a7d2b3c5363ce1035
nhnam1224 <nguyenhainam10009000@gmail.com>
2026-04-12 22:11:55 +0700

    menu scene for the game

---
+++ my-pixel-game/src/scenes/MenuScene.js
@@ -0,0 +1,42 @@
+import * as Phaser from 'phaser';
+
+export default class MenuScene extends Phaser.Scene {
+    constructor() {
+        super('MenuScene');
+    }
+
+    create() {
+        this.add.text(400, 150, 'FUNNY HOTEL', {
+            fontSize: '48px',
+            fill: '#8b0000',
+            fontFamily: 'Courier',
+            fontStyle: 'bold'
+        }).setOrigin(0.5);
+
+        this.add.text(400, 200, 'There is no checkout...', {
+            fontSize: '18px',
+            fill: '#555555',
+            fontFamily: 'Courier'
+        }).setOrigin(0.5);
+
+        const playBtn = this.add.text(400, 300, '[ PLAY ]', {
+            fontSize: '32px', fill: '#ffffff', fontFamily: 'Courier'
+        }).setOrigin(0.5).setInteractive({ useHandCursor: true });
+
+        const settingsBtn = this.add.text(400, 380, '[ SETTINGS ]', {
+            fontSize: '32px', fill: '#ffffff', fontFamily: 'Courier'
+        }).setOrigin(0.5).setInteractive({ useHandCursor: true });
+
+        const helpBtn = this.add.text(400, 460, '[ HELP ]', {
+            fontSize: '32px', fill: '#ffffff', fontFamily: 'Courier'
+        }).setOrigin(0.5).setInteractive({ useHandCursor: true });
+
+        const buttons = [playBtn, settingsBtn, helpBtn];
+        buttons.forEach(btn => {
+            btn.on('pointerover', () => btn.setStyle({ fill: '#ff0000' }));
+            btn.on('pointerout', () => btn.setStyle({ fill: '#ffffff' }));
+        });
+
+        playBtn.on('pointerdown', () => this.scene.start('Room101Scene'));
+    }
+}
