bcef06fff78c36509b5c5abc4337bab9a3883e27
nhnam1224 <nguyenhainam10009000@gmail.com>
2026-04-16 20:42:31 +0700

    change image and logic process of bullet

--- my-pixel-game/src/weapons/Bullet.js
+++ my-pixel-game/src/weapons/Bullet.js
@@ -1,35 +1,35 @@
 import * as Phaser from 'phaser';
 
 export default class Bullet extends Phaser.Physics.Arcade.Sprite {
-    constructor(scene, x, y) {
-        // Tạm dùng hình chữ nhật nhỏ làm đạn
-        super(scene, x, y, '');
+    constructor(scene, x, y, frame) {
+        super(scene, x, y, 'fire_bullet', frame); //874
         
         scene.add.existing(this);
         scene.physics.add.existing(this);
         
-        this.graphics = scene.add.graphics();
-        this.graphics.fillStyle(0xffff00, 1); // Đạn màu vàng
-        this.graphics.fillRect(-4, -4, 8, 8); // Kích thước 8x8
-        this.body.setSize(8, 8);
+        this.body.setSize(10, 10);
 
         // Đạn không bị ảnh hưởng bởi trọng lực
         this.body.setAllowGravity(false);
         this.speed = 500;
     }
 
-    fire(x, y, direction) {
+    fire(x, y, angle) {
         this.setPosition(x, y);
-        this.graphics.setPosition(x, y);
+
         this.setActive(true);
         this.setVisible(true);
 
-        // Bắn sang trái hoặc phải tùy vào hướng nhân vật đang quay
-        if (direction === 'left') {
-            this.setVelocityX(-this.speed);
-        } else {
-            this.setVelocityX(this.speed);
-        }
+        this.body.setAllowGravity(false);
+
+        // Tốc độ bay của đạn (bạn có thể đưa biến này vào tham số nếu muốn súng lục đạn bay nhanh, shotgun bay chậm)
+        const speed = 300;
+
+        // PHÉP THUẬT NẰM Ở ĐÂY: Hàm của Phaser tự động tính toán trục X và Y để đạn bay xéo dựa theo góc
+        this.scene.physics.velocityFromAngle(angle, speed, this.body.velocity);
+
+        // Xoay hình ảnh viên đạn cho khớp với hướng bay (nếu viên đạn của bạn là hình dài/xéo)
+        this.setAngle(angle);
 
         // Tự hủy đạn sau 1.5 giây nếu không trúng gì
         this.scene.time.delayedCall(1500, () => {
@@ -39,11 +39,9 @@ export default class Bullet extends Phaser.Physics.Arcade.Sprite {
 
     preUpdate(time, delta) {
         super.preUpdate(time, delta);
-        this.graphics.setPosition(this.x, this.y);
     }
 
     destroyBullet() {
-        this.graphics.destroy();
         this.destroy();
     }
 }
