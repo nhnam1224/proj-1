4f42d26c46e5d08f07da95576b2cdb5530630055
nhnam1224 <nguyenhainam10009000@gmail.com>
2026-04-18 22:34:47 +0700

    add images

--- my-pixel-game/src/scenes/PreloadScene.js
+++ my-pixel-game/src/scenes/PreloadScene.js
@@ -16,31 +16,26 @@ export default class PreloadScene extends Phaser.Scene {
         // TẢI TẤT CẢ HÌNH ẢNH, ÂM THANH CỦA TOÀN BỘ GAME Ở ĐÂY
         // ----------------------------------------------------
         
-        // 1. Tải hình nhân vật
-        this.load.spritesheet('player_img', 'characters/main/ActionDude-Spritesheet1.png', { 
-            frameWidth: 16, 
-            frameHeight: 16 
-        });
+        // --- CÁC FRAME CỦA SOLDIER (Kích thước mỗi frame là 100x100 theo tên folder) ---
+        this.load.spritesheet('soldier_idle', 'characters/soldiers/Characters(100x100)/Soldier/Soldier with shadows/Soldier-Idle.png', { frameWidth: 100, frameHeight: 100 });
+        this.load.spritesheet('soldier_walk', 'characters/soldiers/Characters(100x100)/Soldier/Soldier with shadows/Soldier-Walk.png', { frameWidth: 100, frameHeight: 100 });
+        this.load.spritesheet('soldier_attack', 'characters/soldiers/Characters(100x100)/Soldier/Soldier with shadows/Soldier-Attack01.png', { frameWidth: 100, frameHeight: 100 });
+        this.load.spritesheet('soldier_attack2', 'characters/soldiers/Characters(100x100)/Soldier/Soldier with shadows/Soldier-Attack02.png', { frameWidth: 100, frameHeight: 100 });
+        this.load.spritesheet('soldier_attack3', 'characters/soldiers/Characters(100x100)/Soldier/Soldier with shadows/Soldier-Attack03.png', { frameWidth: 100, frameHeight: 100 });
+        this.load.spritesheet('soldier_death', 'characters/soldiers/Characters(100x100)/Soldier/Soldier with shadows/Soldier-Death.png', { frameWidth: 100, frameHeight: 100 });
+        this.load.spritesheet('soldier_hurt', 'characters/soldiers/Characters(100x100)/Soldier/Soldier with shadows/Soldier-Hurt.png', { frameWidth: 100, frameHeight: 100 });
 
-        // 2. Tải hình viên đạn (Hãy chỉnh lại đường dẫn sao cho đúng với nơi bạn lưu ảnh nhé)
-        this.load.spritesheet('fire_bullet', 'assets/animations/bullets/All_Fire_Bullet_Pixel_16x16.png', { 
-            frameWidth: 16, 
-            frameHeight: 16 
-        });
-
-        // 2. Tải hình súng
-        this.load.image('gun_pistol', 'assets/animations/weapons/M92.png');
-        this.load.image('gun_shotgun', 'assets/animations/weapons/SawedOffShotgun.png');
-        this.load.image('gun_rifle', 'assets/animations/weapons/AK47.png');
-
-        // 3. Tải hình đạn tương ứng
-        this.load.image('ammo_pistol', 'assets/animations/bullets/PistolAmmoSmall.png');
-        this.load.image('ammo_shotgun', 'assets/animations/bullets/ShotgunShellSmall.png');
-        this.load.image('ammo_rifle', 'assets/animations/bullets/RifleAmmoSmall.png');
+        // --- CÁC FRAME CỦA ORC ---
+        this.load.spritesheet('orc_idle', 'characters/soldiers/Characters(100x100)/Orc/Orc with shadows/Orc-Idle.png', { frameWidth: 100, frameHeight: 100 });
+        this.load.spritesheet('orc_walk', 'characters/soldiers/Characters(100x100)/Orc/Orc with shadows/Orc-Walk.png', { frameWidth: 100, frameHeight: 100 });
+        this.load.spritesheet('orc_hurt', 'characters/soldiers/Characters(100x100)/Orc/Orc with shadows/Orc-Hurt.png', { frameWidth: 100, frameHeight: 100 });
+        this.load.spritesheet('orc_death', 'characters/soldiers/Characters(100x100)/Orc/Orc with shadows/Orc-Death.png', { frameWidth: 100, frameHeight: 100 });
+        this.load.spritesheet('orc_attack', 'characters/soldiers/Characters(100x100)/Orc/Orc with shadows/Orc-Attack01.png', { frameWidth: 100, frameHeight: 100 });
+        this.load.spritesheet('orc_attack2', 'characters/soldiers/Characters(100x100)/Orc/Orc with shadows/Orc-Attack02.png', { frameWidth: 100, frameHeight: 100 });
 
         // 3. Tải hình kẻ địch, âm thanh, v.v... sau này thêm vào đây
+        this.load.spritesheet('arrow', 'characters/soldiers/Arrow(Projectile)/Arrow01(100x100).png', { frameWidth: 100, frameHeight: 100 });
     }
-
     create() {
         // Ngay khi preload() chạy xong 100%, hàm create() sẽ được gọi
         // Lúc này dữ liệu đã sẵn sàng, ta chuyển người chơi sang màn hình Menu
